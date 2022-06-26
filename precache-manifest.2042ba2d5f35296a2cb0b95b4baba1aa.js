9e4e94bf05882048992cb6cb3378dfd872d5699d
chayan07 <pathakchayan20@gmail.com>
2022-06-26 16:39:07 +0530

    Updates

---
+++ precache-manifest.2042ba2d5f35296a2cb0b95b4baba1aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ace819aff843c584ae4",
+    "url": "/digital-clock-react/static/css/main.b30bf8fb.chunk.css"
+  },
+  {
+    "revision": "0ace819aff843c584ae4",
+    "url": "/digital-clock-react/static/js/main.0ace819a.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/digital-clock-react/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "49d0ec46fe87698d2ee6",
+    "url": "/digital-clock-react/static/js/2.49d0ec46.chunk.js"
+  },
+  {
+    "revision": "fde199e6ed3f8152e092",
+    "url": "/digital-clock-react/static/js/runtime~main.fde199e6.js"
+  },
+  {
+    "revision": "7cf5a7a8f3c786903f8df1283b3985e3",
+    "url": "/digital-clock-react/index.html"
+  }
+];
